15cae3702e7081a045b89d7f7e8bb6595ea1632e
Billy Joe KOM KAMZE <billykamze3@gmail.com>
2022-04-25 17:48:43 +0200

    Première version de Chatroom

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<title>Bienvenue  sur Chatroom - Sin up, Log in, Chat </title>
+		<link rel="stylesheet" type="text/css" href="css/index.css">
+	</head>
+
+<body>
+
+	<div id="header">
+		<div class="head-view">
+			<ul>
+				<li><b>Chatroom</b></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li><a href="signin.php" title="Sign in"><button class="btn-sign-in" value="Sign in">Sign in</button></a></li>
+				<li><a href="signup.php" title="Sign up"><button class="btn-sign-up" value="Sign up">Sign up</button></a></li>
+			</ul>
+		</div>
+	</div>
+
+	<div id="container">
+		<div class="image-display">
+			<p style="padding-top: 200px;">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam nihil recusandae rem dolor aut, sed saepe culpa tenetur ipsam laudantium quod voluptatem. Asperiores assumenda est id. Enim consequatur voluptatum tempora illum aspernatur iste incidunt iusto itaque, excepturi, est aperiam quo nemo vitae veritatis consectetur! Hic, animi? Provident dolorum fugiat itaque maiores commodi velit quisquam officia perspiciatis est? Nemo nobis temporibus fugiat omnis ratione iusto vitae! Quaerat ipsam, rem adipisci totam, nemo veniam excepturi quam illum laborum voluptatem consectetur, quasi possimus. Nisi, repudiandae expedita ad laboriosam labore odio sunt obcaecati. Deleniti maxime recusandae praesentium exercitationem architecto voluptas nesciunt molestias eum velit?</p>
+				<!-- <img src="image/wold.jpg" class="img-style" /> -->
+		</div>
+	</div>
+
+</body>
+
+</html>
